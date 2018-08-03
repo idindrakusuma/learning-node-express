bd2d80efe27ef6920839cee245a686fa41f9dd6d
idindrakusuma <indrakusuma.udinus@gmail.com>
2018-08-03 23:17:37 +0700

    add example route

--- server.js
+++ server.js
@@ -3,7 +3,29 @@ const express = require('express');
 var app = express();
 
 app.get('/', (req, res) => {
-  res.send('Express running!')
+  res.send({
+    success: true,
+    message: 'Server express running..'
+  })
+});
+
+app.get('/about', (req, res) => {
+  res.send({
+    success: true,
+    message: 'Showing your information..',
+    data: {
+      name: 'Indra Kusuma',
+      age: 21,
+      website: 'https://blog.indrakusuma.web.id'
+    }
+  })
+});
+
+app.get('/error', (req, res) => {
+  res.send({
+    success: false,
+    message: 'error message..'
+  })
 })
 
 app.listen(3000, () => {
